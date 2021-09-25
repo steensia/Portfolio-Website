5befa4ce7ea7497dc90710636dc8dba5da33463c
Steen <steensia101@gmail.com>
2021-09-24 23:34:55 -0600

    Finished Body-Skills and starting Body-Work component

--- portfolio/src/Components/Body/Skills/Skills.js
+++ portfolio/src/Components/Body/Skills/Skills.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import Separator from '../../Common/Separator/Separator'
+import { SkillsData } from '../../Data/Skills'
+import SkillCard from './Skill-Card';
+import './Skills.css'
 
 function Skills() {
-    return <div>This is skills</div>
+    const data = SkillsData;
+    return <div className="skills">
+        <Separator/>
+        <label className="section-title">Skills</label>
+        <div className="skills-container">
+            {data.map((skills) => {
+                return (
+                    <div className="skills-section">
+                        <label className="skills-section-title">{skills.type}</label>
+                        <div className="skills-list">
+                            {skills.list.map((skill) => {
+                                return <SkillCard skill={skill}/>
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
 }
 
 export default Skills
